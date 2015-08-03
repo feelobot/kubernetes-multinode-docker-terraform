fbcaccd68740e9d8827ca1888487e0bf082fc06e
Felix Rodriguez <felix.a.rod@gmail.com>
2015-08-03 16:42:26 -0700

    Update aws.tf

--- aws.tf
+++ aws.tf
@@ -1,9 +1,16 @@
 # CoreOS
 variable "ami" {
-    description = "the AMI to use"
-   default = "ami-04a2766c"
+  description = "the AMI to use"
+  default = "ami-04a2766c"
 }
+
 resource "aws_instance" "master" {
-    ami = "${var.ami}"
-    instance_type = "m1.small"
+  ami = "${var.ami}"
+  instance_type = "m1.small"
+}
+
+resource "aws_instance" "worker" {
+  ami = "${var.ami}"
+  instance_type = "m1.small"
+  count = 2
 }
